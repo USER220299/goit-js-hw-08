20e2c222d3ff7085d25945be49b149c3fcf26246
USER220299 <127625631+USER220299@users.noreply.github.com>
2023-07-28 22:40:11 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,19 +2,13 @@ import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 
-const player = new Player('vimeo-player', {
-    id: 19231868,
-    width: 640
-});
+const player = new Player(iframe);
 
 player.on('play', function() {
     console.log('played the video!');
 });
 
- const options = {
-        width: 640,
-        loop: true
-    };
+ 
 // const player = new Player('handstick', {
 //     id: 19231868,
 //     width: 640
