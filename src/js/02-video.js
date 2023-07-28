c705cd6eaabea18358732e39370689de2c9a2800
USER220299 <127625631+USER220299@users.noreply.github.com>
2023-07-28 21:10:36 +0300

    solution hw

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,5 +1,25 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+const iframe = document.querySelector('iframe');
 
+const player = new Player('vimeo-player', {
+    id: 19231868,
+    width: 640
+});
 
-const qwe = "asdf"
-const name ="hello"
+player.on('play', function() {
+    console.log('played the video!');
+});
 
+ const options = {
+        width: 640,
+        loop: true
+    };
+// const player = new Player('handstick', {
+//     id: 19231868,
+//     width: 640
+// });
+
+// player.on('play', function() {
+//     console.log('played the video!');
+// });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+console.log(form);
+
+const key = "feedback-form-state";
+const value = {}
+
+form.addEventListener('input', throttle(onFormInput,  500));
+form.addEventListener('submit', onFormSubmit);
+
+populalForm() 
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    event.target.reset();
+
+    console.log(JSON.parse(localStorage.getItem(key)));
+      localStorage.removeItem(key);
+}
+
+function onFormInput(event) {
+     event.preventDefault();
+  value[event.target.name] = event.target.value;
+   localStorage.setItem(key, JSON.stringify(value));
+
+}
+
+function populalForm() {
+    const saveValue = JSON.parse(localStorage.getItem(key));
+    if (saveValue) {
+        let { email, message } = form.elements;
+        email.value = saveValue.email;
+        message.value = saveValue.message;
+        
+    }
+
+}
